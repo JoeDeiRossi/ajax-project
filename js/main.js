a3826fea31ac40300f8c8e52e1152685acef8348
Joe Dei Rossi <deirossijoseph@gmail.com>
2022-06-21 00:38:20 +0000

    No functional changes, refactored code for efficiency

--- js/main.js
+++ js/main.js
@@ -3,83 +3,19 @@ const firstResults = document.querySelector('.results.first');
 const secondResults = document.querySelector('.results.second');
 
 searchButtons[0].addEventListener('click', () => {
-  const searchedMovie = document.querySelector('input.first');
-  searchedMovie.style.display = 'none';
-  searchButtons[0].style.display = 'none';
-  while (firstResults.hasChildNodes()) {
-    firstResults.removeChild(firstResults.firstChild);
-  }
-
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=b638ba27b8c344059fe14ec066253c2e&query=${searchedMovie.value}`);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', () => {
-    const results = xhr.response.results;
-    for (let i = 0; i < 5; i++) {
-      const currentResult = results[i];
-      const searchResult = document.createElement('li');
-      searchResult.setAttribute('id', results[i].id);
-
-      const rowDiv = document.createElement('div');
-      rowDiv.setAttribute('class', 'row');
-      searchResult.appendChild(rowDiv);
-
-      const title = document.createElement('p');
-      title.setAttribute('class', 'result-text');
-      title.textContent = currentResult.title;
-      rowDiv.appendChild(title);
-
-      firstResults.appendChild(searchResult);
-
-      searchResult.addEventListener('click', event => {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://api.themoviedb.org/3/movie/${searchResult.getAttribute('id')}/credits?api_key=b638ba27b8c344059fe14ec066253c2e`);
-        xhr.responseType = 'json';
-        xhr.addEventListener('load', () => {
-          const cast = xhr.response.cast;
-
-          while (firstResults.hasChildNodes()) {
-            firstResults.removeChild(firstResults.firstChild);
-          }
-
-          const castDropdown = document.createElement('li');
-          castDropdown.className = 'dropdown';
-
-          const rowDiv = document.createElement('div');
-          rowDiv.setAttribute('class', 'row');
-          castDropdown.appendChild(rowDiv);
-
-          const castHeader = document.createElement('h2');
-          castHeader.textContent = 'Cast';
-          rowDiv.appendChild(castHeader);
-
-          const castContainer = document.createElement('li');
-          castContainer.className = 'cast-container';
-
-          for (let i = 0; i < cast.length; i++) {
-            const castMember = document.createElement('div');
-            castMember.className = 'cast-member';
-            castMember.textContent = cast[i].name + ' as ' + cast[i].character;
-            castContainer.appendChild(castMember);
-          }
-
-          firstResults.appendChild(castDropdown);
-          firstResults.appendChild(castContainer);
-        });
-        xhr.send();
-      });
-    }
-  });
-  xhr.send();
+  handleSearchClick('first');
 });
 
 searchButtons[1].addEventListener('click', () => {
-  const searchedMovie = document.querySelector('input.second');
+  handleSearchClick('second');
+});
+
+function handleSearchClick(searchWindow) {
+  const index = searchWindow === 'first' ? 0 : 1;
+  searchButtons[index].style.display = 'none';
+
+  const searchedMovie = document.querySelector(`input.${searchWindow}`);
   searchedMovie.style.display = 'none';
-  searchButtons[1].style.display = 'none';
-  while (secondResults.hasChildNodes()) {
-    secondResults.removeChild(secondResults.firstChild);
-  }
 
   var xhr = new XMLHttpRequest();
   xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=b638ba27b8c344059fe14ec066253c2e&query=${searchedMovie.value}`);
@@ -100,46 +36,57 @@ searchButtons[1].addEventListener('click', () => {
       title.textContent = currentResult.title;
       rowDiv.appendChild(title);
 
-      secondResults.appendChild(searchResult);
+      searchWindow === 'first' ? firstResults.appendChild(searchResult) : secondResults.appendChild(searchResult);
 
       searchResult.addEventListener('click', event => {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://api.themoviedb.org/3/movie/${searchResult.getAttribute('id')}/credits?api_key=b638ba27b8c344059fe14ec066253c2e`);
-        xhr.responseType = 'json';
-        xhr.addEventListener('load', () => {
-          const cast = xhr.response.cast;
-
-          while (secondResults.hasChildNodes()) {
-            secondResults.removeChild(secondResults.firstChild);
-          }
-
-          const castDropdown = document.createElement('li');
-          castDropdown.className = 'dropdown';
-
-          const rowDiv = document.createElement('div');
-          rowDiv.setAttribute('class', 'row');
-          castDropdown.appendChild(rowDiv);
-
-          const castHeader = document.createElement('h2');
-          castHeader.textContent = 'Cast';
-          rowDiv.appendChild(castHeader);
-
-          const castContainer = document.createElement('li');
-          castContainer.className = 'cast-container';
-
-          for (let i = 0; i < cast.length; i++) {
-            const castMember = document.createElement('div');
-            castMember.className = 'cast-member';
-            castMember.textContent = cast[i].name + ' as ' + cast[i].character;
-            castContainer.appendChild(castMember);
-          }
-
-          secondResults.appendChild(castDropdown);
-          secondResults.appendChild(castContainer);
-        });
-        xhr.send();
+        handleResultClick(searchWindow, searchResult);
       });
     }
   });
   xhr.send();
-});
+}
+
+function handleResultClick(searchWindow, searchResult) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://api.themoviedb.org/3/movie/${searchResult.getAttribute('id')}/credits?api_key=b638ba27b8c344059fe14ec066253c2e`);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', () => {
+    const cast = xhr.response.cast;
+
+    const castDropdown = document.createElement('li');
+    castDropdown.className = 'dropdown';
+
+    const rowDiv = document.createElement('div');
+    rowDiv.setAttribute('class', 'row');
+    castDropdown.appendChild(rowDiv);
+
+    const castHeader = document.createElement('h2');
+    castHeader.textContent = 'Cast';
+    rowDiv.appendChild(castHeader);
+
+    const castContainer = document.createElement('li');
+    castContainer.className = 'cast-container';
+
+    for (let i = 0; i < cast.length; i++) {
+      const castMember = document.createElement('div');
+      castMember.className = 'cast-member';
+      castMember.textContent = cast[i].name + ' as ' + cast[i].character;
+      castContainer.appendChild(castMember);
+    }
+
+    if (searchWindow === 'first') {
+      while (firstResults.hasChildNodes()) {
+        firstResults.removeChild(firstResults.firstChild);
+      }
+      firstResults.appendChild(castDropdown);
+      firstResults.appendChild(castContainer);
+    } else {
+      while (secondResults.hasChildNodes()) {
+        secondResults.removeChild(secondResults.firstChild);
+      }
+      secondResults.appendChild(castDropdown);
+      secondResults.appendChild(castContainer);
+    }
+  });
+  xhr.send();
+}
